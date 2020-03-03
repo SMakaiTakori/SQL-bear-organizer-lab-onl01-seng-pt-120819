0be25f163bc820b75e5146354e7484f3fe01f9c2
S. Makai Takori <makait87@gmail.com>
2020-03-03 00:57:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,5 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+
 ("Mr. Chocolate", 5, "M", "brown", "happy", 1),
 ("Rowdy", 2, "M", "black", "happy", 1),
 ("Tabitha", 8, "F", "spotted brown", "grumpy", 1),
@@ -7,3 +8,13 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
 ("Grinch", 7, "M", "green", "grumpy", 1),
 ("Wendy", 3, "F", "purple", "excited", 1),
 (NULL, 12, "M", "white", "angry", 0);
+
+-- ("Mr. Chocolate", 5, "M", "brown", "happy", true),
+-- ("Rowdy", 2, "M", "black", "happy", true),
+-- ("Tabitha", 8, "F", "spotted brown", "grumpy", true),
+-- ("Sergeant Brown", 1, "F", "brown", "tired", true),
+-- ("Melissa", 4, "F", "tan", "hungry", true),
+-- ("Grinch", 7, "M", "green", "grumpy", true),
+-- ("Wendy", 3, "F", "purple", "excited", true),
+-- (NULL, 12, "M", "white", "angry", false);
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,11 +11,11 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name,age FROM bears WHERE age = (SELECT MAX (age) FROM bears) AND name IS NOT null; "
+  "SELECT name,age FROM bears ORDER BY age DESC LIMIT 1; "
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name,age FROM bears WHERE age = (SELECT MIN(age) FROM bears);"
+  "SELECT name,age FROM bears ORDER BY age LIMIT 1;;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
@@ -29,3 +29,4 @@ end
 def selects_bear_that_killed_Tim
   "SELECT * FROM bears WHERE name IS NULL;"
 end
+
